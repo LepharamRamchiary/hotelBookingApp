680058c08b8bdb83873dec091c00d1aab30b366a
Lepha <lepharamchiary@gmail.com>
2023-04-26 13:09:59 +0530

    create a new user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,30 @@
 import express from "express";
+import { updateUser, deleteUser, getUser, getAllUser } from "../controllers/user.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+// router.get("/checkauthentaioation", verifyToken, (req, res, next) =>{
+//     res.send("Hello user, you are logged in");
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) =>{
+//     res.send("Hello user, you are logged in amd you can delete you account");
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) =>{
+//     res.send("Hello Admin, you are logged in amd you can delete all accounts");
+// });
+
+
+// UPDATE
+router.put("/:id", verifyUser, updateUser);
+// DELETE
+router.delete("/:id", verifyUser, deleteUser);
+// GET
+router.get("/:id", verifyUser, getUser);
+// GET ALL
+router.get("/", verifyAdmin, getAllUser);
 
 
 export default router
