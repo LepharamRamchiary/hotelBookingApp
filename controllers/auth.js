f9eb7314b1437a56b57a90bc35eb2688555dedf0
Lepha <lepharamchiary@gmail.com>
2023-04-26 13:05:26 +0530

    create a cookie access_token

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,20 +26,22 @@ export const register = async (req, res, next) => {
 // LOGIN
 export const login = async (req, res, next) => {
     try {
-        const user = await User.findOne({username: req.body.username});
-        if(!user) return next(createError(404, "User not found!"));
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) return next(createError(404, "User not found!"));
 
         const isPasswordCorrect = await bcrypt.compare(
-            req.body.password, 
+            req.body.password,
             user.password
         );
-        if(!isPasswordCorrect) 
-          return next(createError(400,"Wrong password Or username!"));
+        if (!isPasswordCorrect)
+            return next(createError(400, "Wrong password Or username!"));
 
-          const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin}, "lepha333")
-        
+        const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
+
         const { password, isAdmin, ...otherDetails } = user._doc;
-        res.status(201).json({...otherDetails});
+        res.cookie("access_token", token, {
+            httpOnly: true,
+        }).status(201).json({ ...otherDetails });
     } catch (err) {
         next(err);
     }
