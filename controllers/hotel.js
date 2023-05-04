71edbbba26b9b935e1f9740d0e4939641d048c61
Lepha <lepharamchiary@gmail.com>
2023-05-04 21:51:49 +0530

    .

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -57,7 +57,7 @@ export const countByCity = async (req, res, next) => {
     const cities = req.query.cities.split(",");
     try {
         const list = await Promise.all(cities.map(city => {
-            return Hotel.countDocuments({city: city})
+            return Hotel.countDocuments({ city: city })
         }))
         res.status(200).json(list)
     } catch (err) {
@@ -66,9 +66,21 @@ export const countByCity = async (req, res, next) => {
 }
 
 export const countByType = async (req, res, next) => {
+
     try {
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels)
+        const hotelCount = await Hotel.countDocuments({ type: "hotel" });
+        const apartmentCount = await Hotel.countDocuments({ type: "apartment" });
+        const resortCount = await Hotel.countDocuments({ type: "resort" });
+        const villaCount = await Hotel.countDocuments({ type: "villa" });
+        const cabinCount = await Hotel.countDocuments({ type: "cabin" });
+
+        res.status(200).json([
+            { type: "hotel", count: hotelCount },
+            { type: "apartments", count: apartmentCount },
+            { type: "resorts", count: resortCount },
+            { type: "villas", count: villaCount },
+            { typr: "cabins", count: cabinCount }
+        ]);
     } catch (err) {
         next(err)
     }
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
-import { countByCity, createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controllers/hotel.js";
+import { countByCity, countByType, createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -17,7 +17,9 @@ router.delete("/:id", verifyAdmin, deleteHotel);
 router.get("/find/:id", getHotel);
 // GET ALL
 router.get("/", getAllHotel);
+
 router.get("/countByCity", countByCity);
-// router.get("/countByType", getAllHotel);
+
+router.get("/countByType", countByType);
 
 export default router 
