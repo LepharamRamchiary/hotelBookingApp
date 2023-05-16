56c099df0e22a27597b5ef77e79e2da9e2f89c66
Lepha <lepharamchiary@gmail.com>
2023-05-16 12:10:06 +0530

    modify

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -50,7 +50,7 @@ export const getAllHotel = async (req, res, next) => {
         const hotels = await Hotel.find({
             featured,
             ...others,
-            cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+            cheapestPrice: { $gt: min || 1, $lt: max || 999 },
         }).limit(req.query.limit);
         res.status(200).json(hotels);
     } catch (err) {
